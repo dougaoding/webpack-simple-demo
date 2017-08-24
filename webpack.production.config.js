769ef3956f3efe2fa52f35915048895e25d120dd
mambaNeverOut <skater_firefox@126.com>
2017-08-24 14:17:20 +0800

    说明说明

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
     output: {
         path: __dirname + "/build", //打包后的文件存放的地方
-        filename: "bundle.js" //打包后输出文件的文件名
+        //添加缓存
+        filename: "bundle-[hash].js" //打包后输出文件的文件名
     },
     //添加服务器
     devServer: {
